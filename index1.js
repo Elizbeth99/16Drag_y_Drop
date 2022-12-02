9a9e5a54f6592440693b381007564405a67b801e
Eli <113862594+Elizbeth99@users.noreply.github.com>
2022-12-02 07:48:12 -0600

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,65 @@
+const parrafos = document.querySelectorAll(".parrafo")
+const secciones = document.querySelectorAll(".seccion")
+
+parrafos.forEach
+(parrafo => 
+    {
+     parrafo.addEventListener
+      ("dragstart", event => 
+      {
+        console.log("Se está arrastrando el párrafo: " + parrafo.innerText)
+        parrafo.classList.add("dragging")
+        event.dataTransfer.setData("id", parrafo.id)
+        const elemento_fant = document.querySelector(".imagen-fant")
+        event.dataTransfer.setDragImage(elemento_fant, 0, 0)
+       }
+       )
+
+     parrafo.addEventListener
+      ("dragend", () => 
+      {
+        parrafo.classList.remove("dragging")
+      }
+      )
+    }
+)
+
+secciones.forEach
+(seccion => 
+{
+    seccion.addEventListener
+    ("dragover", event => 
+    {
+        event.preventDefault()
+        event.dataTransfer.dropEffect = "copy" 
+    }
+    )
+
+    seccion.addEventListener
+    ("drop", event => 
+    {
+        console.log("Drop")
+        const id_parrafo = event.dataTransfer.getData("id")
+        const parrafo = document.getElementById(id_parrafo)
+        seccion.appendChild(parrafo)
+    }
+    )
+}
+)
+
+const papelera = document.querySelector(".papelera")
+papelera.addEventListener
+("dragover", event => 
+{
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "copy"
+}
+)
+
+papelera.addEventListener
+("drop", event => 
+{
+    const id_parrafo = event.dataTransfer.getData("id")
+    document.getElementById(id_parrafo).remove()
+}
+)
